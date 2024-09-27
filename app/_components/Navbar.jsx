f33baf8de5623a4533431662ae8972962d8f8638
Dhillon-Saab <ad70138@gmail.com>
2024-09-27 16:49:20 -0600

    changed the dynamic functionality of Navbar

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -1,8 +1,16 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import { usePathname } from "next/navigation";
+import React, { useState } from "react";
 
 function Navbar() {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
     <header className=" bg-navbar-bg sticky top-0 overflow-hidden drop-shadow-lg shadow-md">
       <div className="mx-auto  max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
@@ -21,22 +29,33 @@ function Navbar() {
           </div>
 
           <div className="flex items-center gap-4">
-            <div className="sm:hidden">
-              <label htmlFor="Tab" className="sr-only">
-                Tab
-              </label>
+            <button
+              onClick={toggleMenu}
+              className="text-white inline-block md:hidden"
+            >
+              <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M4 5h16a1 1 0 010 2H4a1 1 0 110-2zm0 6h16a1 1 0 010 2H4a1 1 0 010-2zm0 6h16a1 1 0 010 2H4a1 1 0 010-2z"
+                />
+              </svg>
+            </button>
 
-              <select id="Tab" className="w-full rounded-md border-gray-200">
-                <option>Settings</option>
-                <option>Messages</option>
-                <option>Archive</option>
-                <option select>Notifications</option>
-              </select>
-            </div>
-
-            <div className="hidden sm:block">
+            <div className={`${
+              isOpen ? "hidden" : "block"
+            } flex flex-col md:flex-row md:space-x-8 mt-4 md:mt-0 gap-7`}>
               <nav className="flex gap-6" aria-label="Tabs">
-                <Link
+
+              {
+                ["/", "/works", "/services", "/contactUs"].map((path)=>(
+                  <Link key={path} href={path} className={`${path === pathname ? 'bg-sky-100 p-2 text-sm font-medium text-navbar-bg' : 'p-2 text-sm font-medium text-white hover:bg-gray-50 hover:text-gray-700'} shrink-0 rounded-lg`}>
+                    {path === '/' ? "Home" : path.substring(1).charAt(0).toUpperCase() + path.substring(2)}
+                  </Link>
+                ))
+              }
+
+                {/* <Link
                   href="/"
                   className="shrink-0 rounded-lg p-2 text-sm font-medium text-white hover:bg-gray-50 hover:text-gray-700"
                 >
@@ -62,7 +81,7 @@ function Navbar() {
                   aria-current="page"
                 >
                   Contact Us
-                </Link>
+                </Link> */}
               </nav>
             </div>
           </div>
