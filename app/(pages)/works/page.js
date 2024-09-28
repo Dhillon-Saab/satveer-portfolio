c212cf2967b4f03fdbb171cc3cfbd5f1575c3bb2
Dhillon-Saab <ad70138@gmail.com>
2024-09-27 21:07:39 -0600

    Under Dev Message

---
+++ app/(pages)/works/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='text-4xl text-center my-64 justify-center items-center'>This feature is under-development. Thanks for patience</div>
+  )
+}
+
+export default page
