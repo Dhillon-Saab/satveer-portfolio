7be3b33b21952f5e8b6a26d9db5dd2890a002310
Dhillon-Saab <ad70138@gmail.com>
2025-02-09 00:09:36 -0700

    Changes to the work Dynamics for better mobile feature

--- app/(pages)/works/[work]/page.js
+++ app/(pages)/works/[work]/page.js
@@ -60,8 +60,8 @@ function Page({ params }) {
             duration={workData?.content?.About?.duration?.toString()}
             tools={workData?.content?.About?.tools}
           />
-          <QuestionsChallenges />
-          <Solution />
+          {/* <QuestionsChallenges />
+          <Solution /> */}
         </>
       ) : (
         <section className="bg-gray-800 relative place-items-center grid h-screen w-screen gap-4">
