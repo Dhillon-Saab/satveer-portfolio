0db2b3d21d1cc2be45906ad6f903440e2a9598fd
Dhillon-Saab <ad70138@gmail.com>
2025-02-09 16:20:10 -0700

    color optimization for the loading screen

--- app/(pages)/works/page.js
+++ app/(pages)/works/page.js
@@ -31,7 +31,7 @@ function Page() {
           ))}
         </div>
       ) : (
-        <section className="bg-gray-800 relative place-items-center grid h-screen w-screen gap-4">
+        <section className="bg-navbar-bg relative place-items-center grid h-screen w-screen gap-4">
           <div className="bg-gray-500 w-48 h-48  absolute animate-ping rounded-full delay-5s shadow-xl"></div>
 
           <div className="bg-gray-400 w-32 h-32 absolute animate-ping rounded-full shadow-xl"></div>
